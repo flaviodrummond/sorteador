183ac3f0b3c5ee6c0d3579ca5868d22666fe0c14
flaviodrummond <flaviosax20@gmail.com>
2025-05-04 01:30:19 -0300

    Conclusão da parte lógica do sorteador, inclusão de novas img

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,27 @@
 const button = document.querySelector("#sortear")
+const resultado = document.querySelector(".result-value")
 
 
 button.addEventListener('click', function () {
-    console.log("cliquei")
-})
+    const min = Math.ceil(document.querySelector(".min").value)
+    const max = Math.floor (document.querySelector(".max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (min >= max) {
+        alert('Valor mínimo tem que ser MENOR que o valor máximo');
+        resultado.style.display = 'none';
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        // Mostra o valor sorteado
+        resultado.innerHTML = result;
+
+        // Alterna a visibilidade
+        if (resultado.style.display === 'block') {
+            resultado.style.display = 'none';
+        } else {
+            resultado.style.display = 'block';
+        }
+    }
+});
